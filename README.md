242766f2aa2c799d2e5a29f0dc04bc8e5a62a0d1
Midnight Rambler <midnightrambler.dev@gmail.com>
2025-02-24 15:07:00 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rusts
+Porting some of Rust's standard library to Typescript
+
+The current modules that have been implemented are:
+- Option as `rusts/option`
+- Result as `rusts/result`
+- Iterator as `rusts/iter`
---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		environment: "node",
+
+		globals: true,
+
+		include: ["tests/**/*.{test,spec}.ts"],
+
+		coverage: {
+			provider: "v8",
+
+			include: [],
+
+			exclude: ["src/**/types/**/*.ts", "src/**/types.ts"],
+
+			thresholds: {
+				lines: 100,
+				statements: 100,
+				branches: 100,
+				functions: 100,
+			},
+		},
+	},
+});
